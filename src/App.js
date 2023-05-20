bfeeb5d96988518145d54fa753984ea9aa156d04
powerwlsl <hyejinahn0619@gmail.com>
2023-05-20 17:48:23 -0500

    added react query

--- src/App.js
+++ src/App.js
@@ -4,25 +4,30 @@ import Dropzone from './components/Dropzone';
 import Layout from './Layout';
 import { useState } from 'react';
 import { Tooltip, Button } from "@material-tailwind/react";
-
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 function App() {
 
   const [files, setFiles] = useState([]);
 
   return (
-    <Layout children={<>
-      <h1 className='text-3xl font-bold leading-tight tracking-tight text-gray-900 mb-4'>
-        Lung Cancer
-      </h1>
-      <Tooltip content="Material Tailwind">
-        <button className="bg-blue-500 text-white font-bold rounded-full px-2 py-0">
-          ?
-        </button>
-      </Tooltip>
-      <Dropzone setFiles={setFiles} files={files} />
-    </>}>
+    <QueryClientProvider client={new QueryClient()}>
+      <Layout children={<>
+        <h1 className='text-3xl font-bold leading-tight tracking-tight text-gray-900 mb-4'>
+          Lung Cancer
+        </h1>
+        <Tooltip content="Material Tailwind">
+          <button className="bg-blue-500 text-white font-bold rounded-full px-2 py-0">
+            ?
+          </button>
+        </Tooltip>
+        <Dropzone setFiles={setFiles} files={files} />
+      </>}>
 
-    </Layout>
+      </Layout>
+    </QueryClientProvider>
   );
 }
 
